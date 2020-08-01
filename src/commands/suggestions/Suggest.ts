055d4527023d42afd1ade4b006dfd1b34b5f0779
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-01 15:18:44 +1200

    Added suggestion features

---
+++ src/commands/suggestions/Suggest.ts
@@ -0,0 +1,54 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message, GuildChannel, TextChannel, MessageEmbed } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+import { count } from "console";
+
+export default class SuggestionChannelCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "suggest",
+            group: "suggestions",
+            memberName: "suggest",
+            description: "Create an anonymous suggestion!",
+            userPermissions: ["ADMINISTRATOR"],
+            guildOnly: true,
+            args: [
+                {
+                    key: "suggestion",
+                    prompt: "Please write your suggestion.",
+                    type: "string",
+                    validate: (m: string) => m.length < 1000,
+                    error: "Please make sure your suggestion is less than 1000 characters long.",
+                },
+            ],
+        });
+    }
+
+    async run(message: CommandoMessage, { suggestion }: { suggestion: string }): Promise<Message> {
+        const guildSettings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+        const channel = guildSettings.getSuggestionChannel();
+        if (channel == undefined) {
+            return message.channel.send(
+                "This server does not have a suggestion channel. If you would like one, ask an administrator to create one!"
+            );
+        }
+        const channelObj = message.guild.channels.resolve(channel);
+        if (!channelObj) {
+            return message.channel.send(
+                "This servers suggestion channel appears to have been deleted. Please ask an administrator to reset it!"
+            );
+        }
+        const counter = await guildSettings.incrementSuggestions();
+        const output = new MessageEmbed()
+            .setAuthor(
+                "Anonymous",
+                "https://cdn.discordapp.com/attachments/737561730967928853/738955669562065006/question-mark-emoji-by-twitter.png"
+            )
+            .setTitle(`Suggestion #${counter}`)
+            .setDescription(suggestion);
+        const msg = await message.channel.send(output);
+        await msg.react("⬆️");
+        await msg.react("⬇️");
+        return msg;
+    }
+}
---
+++ src/commands/suggestions/SuggestionChannel.ts
@@ -0,0 +1,32 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message, GuildChannel, TextChannel } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+
+export default class SuggestionChannelCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "suggestionchannel",
+            group: "suggestions",
+            memberName: "suggestionchannel",
+            description: "Set-up a suggestion channel where user suggestions will be posted.",
+            userPermissions: ["ADMINISTRATOR"],
+            guildOnly: true,
+            args: [
+                {
+                    key: "suggestionChannel",
+                    prompt: "What channel should the suggestion channel be?",
+                    type: "text-channel",
+                },
+            ],
+        });
+    }
+
+    async run(message: CommandoMessage, { suggestionChannel }: { suggestionChannel: GuildChannel }): Promise<Message> {
+        if (suggestionChannel.type != "text") {
+            return message.channel.send("Cannot set the suggestion channel to a non-text channel.");
+        }
+        const guildSettings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+        await guildSettings.setSuggestionChannel({ channelId: suggestionChannel.id });
+        return message.channel.send(`${suggestionChannel} has been set as this servers suggestion channel!`);
+    }
+}
