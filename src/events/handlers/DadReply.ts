3022bd89c060dc84455143c30dbd6a8543541709
Louis Ye <louisye@ymail.com>
2020-08-02 23:57:02 +1200

    Improved variable name.

--- src/events/handlers/DadReply.ts
+++ src/events/handlers/DadReply.ts
@@ -6,10 +6,10 @@ export default class DadReply implements BotEvent {
     private client: Bot = Bot.Get;
 
     start(): void {
-        this.client.on("message", this.updater);
+        this.client.on("message", this.handler);
     }
 
-    private updater = async (message: Message) => {
+    private handler = async (message: Message) => {
         const content = message.content.toLowerCase();
         if(content.startsWith("im ")){
             const string = message.content.slice(3)
