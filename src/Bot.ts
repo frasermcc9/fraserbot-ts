473406ab8d1c0a0e11cd48d667d031f387f8f5e3
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-03 20:15:52 +1200

    Added settings cache and custom command store

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,10 @@
-import { CommandoClient } from "discord.js-commando";
+import { CommandoClient, CommandoMessage } from "discord.js-commando";
 import Log from "./helpers/Log";
 import path from "path";
 import dotenv from "dotenv";
 import { EventManager } from "./EventManager";
-import { ReactionCollector } from "discord.js";
+import { ReactionCollector, MessageCollector as Function, Message } from "discord.js";
+import { ServerSettingsModel, IServerSettingsDocument } from "./database/models/ServerSettings/ServerSettings.model";
 dotenv.config();
 export class Bot extends CommandoClient {
     private static readonly bot: Bot = new Bot();
@@ -22,7 +23,9 @@ export class Bot extends CommandoClient {
             invite: "https://discord.gg/rwFhQ9V",
             disableMentions: "everyone",
         });
-        this.reactionListeners = new Map<string, ReactionCollector>();
+        this.reactionListeners = new Map();
+        this.commandListeners = new Map();
+        this.guildSettingsCache = new Map();
     }
 
     async start(): Promise<void> {
@@ -31,8 +34,9 @@ export class Bot extends CommandoClient {
                 Log.error("Bot", "No token was provided.");
                 return reject(new Error("TOKEN is not provided"));
             }
-            this.once("ready", () => {
+            this.once("ready", async () => {
                 Log.info("Bot", `Bot logged in as ${this.user?.tag}`);
+                await this.refreshCache();
                 this.registerCommands();
                 this.registerEvents();
                 resolve();
@@ -52,9 +56,11 @@ export class Bot extends CommandoClient {
             .registerGroups([
                 ["core", "Core bot commands"],
                 ["moderation", "Moderation Commands"],
-                ["colors", "Colour Commands"],
+                ["roles", "Commands relating to role reactions"],
                 ["fun", "Fun Commands"],
                 ["suggestions", "Suggestion Commands"],
+                ["config", "Commands for bot settings in this server"],
+                ["messages", "Custom commands and quotes"],
             ])
 
             .registerCommandsIn({
@@ -75,7 +81,41 @@ export class Bot extends CommandoClient {
         this.reactionListeners.delete(key);
     }
 
+    private commandListeners: Map<string, (m: CommandoMessage) => void>;
+    public storeCommandListener(fn: (m: CommandoMessage) => void, guildId: string, commandName: string) {
+        const key = guildId + commandName;
+        this.commandListeners.set(key, fn);
+    }
+    public destroyCommandListener(guildId: string, commandName: string) {
+        const key = guildId + commandName;
+        const fn = this.commandListeners.get(key);
+        if (!fn) return;
+        this.off("message", fn);
+        this.commandListeners.delete(key);
+    }
+
     private registerEvents(): void {
         new EventManager().init();
+    }
+
+    private guildSettingsCache: Map<string, IServerSettingsDocument>;
+    public get GuildSettingsCache() {
+        return this.guildSettingsCache;
+    }
+    /**
+     * refreshes the entire settings cache from database.
+     */
+    public async refreshCache() {
+        const data = await ServerSettingsModel.find();
+        data.forEach((doc) => {
+            this.guildSettingsCache.set(doc.guildId, doc);
+        });
+    }
+    /**
+     * Updates the cache at a single position.
+     * @param guild the settings of the guild to update.
+     */
+    public refreshCachePoint(guild: IServerSettingsDocument) {
+        this.guildSettingsCache.set(guild.guildId, guild);
     }
 }
