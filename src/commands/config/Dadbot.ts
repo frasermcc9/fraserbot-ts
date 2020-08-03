5ec07ba7e65c0adf831cead4d3e2f674aed721bf
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-03 20:16:16 +1200

    Added dadbot toggle command

---
+++ src/commands/config/Dadbot.ts
@@ -0,0 +1,25 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+import { Bot } from "../../Bot";
+
+export default class DadbotCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "dadbot",
+            group: "config",
+            memberName: "dadbot",
+            description: "Toggle dadbot being enabled in this server.",
+            userPermissions: ["ADMINISTRATOR"],
+        });
+    }
+
+    async run(message: CommandoMessage): Promise<Message> {
+        const guildSettings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+
+        const current = !(guildSettings.dadBot ?? false);
+        await guildSettings.setDadbot({ setting: current });
+        Bot.Get.refreshCachePoint(guildSettings);
+        return message.channel.send(`Dadbot is now ${current ? "enabled" : "disabled"} in this server!`);
+    }
+}
