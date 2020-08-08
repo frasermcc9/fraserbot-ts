6f7bf095d0c947e41b0a363a68d8357f4c06bfc0
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-09 02:14:15 +1200

    added command that enables the wiki in a server

---
+++ src/commands/wiki/EnableWiki.ts
@@ -0,0 +1,24 @@
+import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Message } from "discord.js";
+import { ServerSettingsModel } from "../../database/models/ServerSettings/ServerSettings.model";
+
+export default class EnableWikiCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: "enablewiki",
+            aliases: ["togglewiki"],
+            group: "wiki",
+            memberName: "enablewiki",
+            description: "Toggle wiki being enabled in this server.",
+            userPermissions: ["ADMINISTRATOR"],
+        });
+    }
+
+    async run(message: CommandoMessage): Promise<Message> {
+        const guildSettings = await ServerSettingsModel.findOneOrCreate({ guildId: message.guild.id });
+
+        const current = !(guildSettings.wiki.enabled ?? false);
+        await guildSettings.setWikiEnabled({ setting: current });
+        return message.channel.send(`Wiki is now ${current ? "enabled" : "disabled"} in this server!`);
+    }
+}
