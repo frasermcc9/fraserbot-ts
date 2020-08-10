703d4be8200f703d265054690407ab5dd38d452a
Cameron McLean <cameron@tragor.com>
2020-08-10 14:05:51 +1200

    73 but better

--- src/commands/fun/73.ts
+++ src/commands/fun/73.ts
@@ -15,6 +15,9 @@ export default class num73 extends Command {
         if (message.deletable) {
             message.delete();
         }
+        for(let i = 0; i< 72; i++){
+            message.channel.send("73");
+        }
         return message.channel.send("73");
     }
 }
