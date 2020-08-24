fe899f6fd0a31433586d5af3fc5d83498c54cca9
Fraser McCallum <fraser@themccallums.net.nz>
2020-08-25 01:50:30 +1200

    fixed imports. Fixed output so it shows proper user rather than id.

--- src/commands/fun/Right.ts
+++ src/commands/fun/Right.ts
@@ -1,6 +1,5 @@
-import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import { Command, CommandoMessage, CommandoClient } from "discord.js-commando";
 import { Message, GuildMember } from "discord.js";
-import { stringify } from "querystring";
 
 export default class Right extends Command {
     constructor(client: CommandoClient) {
@@ -9,33 +8,34 @@ export default class Right extends Command {
             group: "fun",
             memberName: "right",
             description: "If you have an argument, find who's right",
+            guildOnly: true,
             args: [
                 {
                     key: "member",
                     prompt: "Mention the person you have an argument with",
                     type: "member",
-                },]
+                },
+            ],
         });
     }
 
-    async run(message: CommandoMessage,{ member }: { member: GuildMember }): Promise<Message> {        
+    async run(message: CommandoMessage, { member }: { member: GuildMember }): Promise<Message> {
         const num = Math.random();
         let user: string;
-        if(message.author.id == "526559520428654592" || member.id == "526559520428654592"){
+        if (message.author.id == "526559520428654592" || member.id == "526559520428654592") {
             user = "526559520428654592";
-        }
-        else if (message.author.id == "357675387678883840" || member.id == "357675387678883840"){
+        } else if (message.author.id == "357675387678883840" || member.id == "357675387678883840") {
             return message.channel.send("Acir is always wrong");
-        }
-        else{
-            if ( num >= 0.5){
+        } else {
+            if (num >= 0.5) {
                 user = message.author.id;
-            }
-            else{
+            } else {
                 user = member.id;
             }
         }
-    
-         return message.channel.send(`${user} wins the argument cuz they're smarter`);
+
+        const displayName = message.guild.members.resolve(user)?.displayName;
+
+        return message.channel.send(`${displayName} wins the argument cuz they're smarter`);
     }
 }
